159e8894bc1e8ec7b148b4373866715e217bee0b
Scott Weitzner <scottweitzner@gmail.com>
2015-11-12 16:36:24 -0500

    removed cURL response

--- main.php
+++ main.php
@@ -1,8 +1,14 @@
 <?php
 
-    $data = json_encode(array("text" => "Hello " . "@" . $_POST[user_name]));                                                                  	         
+    $voteTopic = array(
+			"text" => "@" . $_POST[user_name] . " would like to vote on: \n" . "*" . $_POST[text] . "*",
+			"mrkdwn" => true
+		);
+
+    $data = json_encode($voteTopic);                                                                  	         
    
 	$ch = curl_init('https://hooks.slack.com/services/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');                                                                                                                                          
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);  
-	curl_exec($ch);                                                                                                                                                                                                                                                                                                                        
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  #removes "ok" response
+	$output = curl_exec($ch);                                                                                                                                                                                                                                                                                                                         
 	curl_close($ch);
